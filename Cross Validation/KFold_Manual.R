c08e89c80742d9c1445090a375d94f897129950d
Soumya Ray <soumya.ray@gmail.com>
2022-03-30 19:43:11 +0800

    refactor kfold, add PD, and restructure files

--- Cross Validation/KFold_Manual.R
+++ Cross Validation/KFold_Manual.R
@@ -6,23 +6,28 @@ cars_data <- mtcars[ , c("mpg", "disp", "hp", "drat")]
 set.seed(349872347)
 cars_data <- cars_data[sample(nrow(cars_data)),]
 
-KFold_sq_err <- function(i, nfold) {
+KFold_sq_err <- function(i, nfold, model, dataset) {
   #create 10 folds
-  folds <- cut(1:nrow(cars_data), breaks=nfold, labels=FALSE)
- 
+  folds <- cut(1:nrow(dataset), breaks=nfold, labels=FALSE)
+  
   test_indices <- which(folds==i, arr.ind=TRUE)
-  test  <- cars_data[test_indices, ]
-  train <- cars_data[-test_indices, ]
-  model <- lm(mpg ~ ., data = train)
+  test  <- dataset[test_indices, ]
+  train <- dataset[-test_indices, ]
+  model <- lm(model, data = train)
   predictions <- predict(model, test)
   (test$mpg-predictions)^2
 }
 
-kfold_rmse <- function(nfold) {
-  sq_err <- unlist(sapply(1:nfold, KFold_sq_err, nfold=nfold))
+kfold_rmse <- function(model, dataset, nfold=NULL) {
+  if (is.null(nfold)) {
+    nfold <- nrow(dataset)
+  }
+  
+  sq_err <- unlist(sapply(1:nfold, KFold_sq_err, nfold=nfold, 
+                          model=model, dataset=dataset))
   sqrt(mean(sq_err))
 }
 
-kfold_rmse(5)
-kfold_rmse(10)
-kfold_rmse(32) # LOOCV
+kfold_rmse(mpg ~ ., outcome='mpg', cars_data, 5)
+kfold_rmse(mpg ~ ., outcome='mpg', cars_data, 10)
+kfold_rmse(mpg ~ ., outcome='mpg', cars_data) # LOOCV
