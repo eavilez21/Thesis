fea0fdbad07cbac5a00e76472bed9d47a0caa179
Soumya Ray <soumya.ray@gmail.com>
2022-04-08 16:38:59 +0800

    Extracts deviant groups/cases from insurance regression

--- predictive_deviance/insurance_pd.R
+++ predictive_deviance/insurance_pd.R
@@ -44,19 +44,53 @@ abline(h=pd_95)
 library(rpart)
 library(rattle)
 library(rpart.plot)
+library(data.tree)
+library(tree)
 # library(RColorBrewer)
-insurance_tree <- rpart(pd_loocv~., data=insurance, minsplit=2, minbucket=1,cp=.00000000000001)
-fancyRpartPlot(insurance_tree,caption=NULL)
+insurance_rpart <- rpart(pd_loocv~., data=insurance, minsplit=2, minbucket=1,cp=.00000000000001)
+fancyRpartPlot(insurance_rpart,caption=NULL)
 
-insurance_tree_struct<-as.Node(insurance_tree,digits = getOption("digits") - 1,use.n=TRUE)
+insurance_data_tree <- as.Node(insurance_rpart, digits = getOption("digits") - 1, use.n=TRUE)
+
+# library(tree)
+# insurance_tree <- tree(pd_loocv~., data=insurance, mindev = 0.000000001)
+
+frame_deviants <- (insurance_rpart$frame$yval <= pd_95["2.5%"]) | (insurance_rpart$frame$yval >= pd_95["97.5%"])
+# nodes_only <- insurance_rpart$frame[insurance_rpart$frame$var != "<leaf>", ]
+all_deviants <- insurance_rpart$frame[frame_deviants, ]
+deviant_nodes <- all_deviants[all_deviants$var != "<leaf>", ]
+
+
+# Using semcoa package to find deviant groups
+library(semcoa)
+insurance_pd <- cbind(insurance, PD=pd_loocv)
+predictions <- list(
+  pd_data = insurance_pd,
+  PD = pd_loocv
+)
+
+dtree <- deviance_tree(predictions)
+
+grouped_deviants <- unname(unlist(dtree$deviant_groups))
+unique_deviants <- dtree$unique_deviants
+all_deviants <- c(grouped_deviants, unique_deviants)
 
 # TODO
+# - 4. Let's remove the deviant groups and rerun the deviance tree
+#   - 4.1. Make a new dataset without all_deviants
+#   - 4.2. Compute PD values for the new dataset
+#   - 4.3. Fit a new rpart tree on it and examine the structure (can use deviance_tree function)
+# - 5. Let's see if the resulting deviance tree has natural large "subgroups"
+
+# DONE
 # - 1. Use "tree" instead of "rpart" to do the deviance tree
 #   - see if we can "traverse" the tree easily from the root
 #     e.g. can we find node 7 from the root
+#   result: no
 # - 2. Try splitting the tree until there is only 1 case in each leaf node (try with "tree" package)
 #   - might have to play with "cp" or complexity parameter
 #   - might have to find a non-visual way of confirming there is only 1 case in each leaf
+#   result: no, can only do with rpart
 # - 3. Deviant Groups: Find nodes whose ultimate leaf cases have average PD in top/bottom 5% extreme
-# - 4. Let's remove the deviant groups and rerun the deviance tree
-# - 5. Let's see if the resulting deviance tree has natural large "subgroups"
+#   - Find the "major" deviant groups (ignore any deviant subgroups)
+#   result: done!
