7cc0b92981ec5016bc90a451743fdc8390dbecf7
Emmy Avilez <avileze@yahoo.com>
2022-07-02 13:52:10 +0800

    OOS MSE

---
+++ OOS_MSE_80_20.R
@@ -0,0 +1,22 @@
+library(caret)
+library(dplyr)
+
+node_2 <- readRDS("node_2.rds")
+data <- new_insurance
+
+training_obs <- node_2$charges %>% createDataPartition(p = 0.8, list = FALSE)
+
+train <- data[training_obs, ]
+test <- data[-training_obs, ]
+
+# Build the linear regression model on the training set
+model <- lm(charges ~ ., data = train)
+
+# Use the model to make predictions on the test set
+predictions <- model %>% predict(test)
+
+#Examine R-squared, RMSE, and MAE of predictions
+data.frame(R_squared = R2(predictions, test$charges),
+           RMSE = RMSE(predictions, test$charges), 
+           MAE = MAE(predictions, test$charges))
+
